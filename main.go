ca4e2159f797b5a9beadca3adb6cfb3f3cb7472a
Jenson <18335940842@163.com>
2022-12-08 23:50:02 +0800

    pix file

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 
 func main() {
 	app := gin.Default()
-	app.Handle("GET", "/ping", func(context *gin.Context) {
-		name := context.DefaultQuery("name", "王二狗")
-		context.Writer.Write([]byte(name))
+	app.Handle("GET", "/ping", func(c *gin.Context) {
+		name := c.DefaultQuery("name", "王二狗")
+		c.Writer.Write([]byte(name))
 	})
 	app.Run()
 }
